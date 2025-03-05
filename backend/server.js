2ff7c2617ca6d74c7876d6db21ddbb505a6d9449
culbrethj <jacksonc1112@outlook.com>
2025-03-05 17:53:06 -0500

    Return main branch to stable state
    Project is now fully setup so now I am moving to branch based work

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
 
@@ -9,12 +8,12 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log("MongoDB connected"))
-    .catch(err => console.log(err));
-
 app.get("/", (req, res) => {
     res.send("API is running...");
+});
+
+app.get("/api", (req, res) => {
+    res.send("API connection made");
 });
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));

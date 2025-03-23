16b32d517ca59a2ee8a8ca5b1273ccf537e64d43
marksweeney3 <mark.sweeney@ufl.edu>
2025-03-23 14:37:39 -0400

    Do npm run dev in terminal for frontend screen to pop up

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,20 +2,20 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 
 function App() {
-  const [message, setMessage] = useState<string>("");
+    const [message, setMessage] = useState<string>("");
 
-  useEffect(() => {
-    axios.get("/api")
-      .then(res => setMessage(res.data))
-      .catch(err => console.error(err));
-  }, []);
+    useEffect(() => {
+        axios.get("/api")
+            .then(res => setMessage(res.data))
+            .catch(err => console.error(err));
+    }, []);
 
-  return (
-    <div>
-      <h1>MERN Stack App</h1>
-      <p>{message}</p>
-    </div>
-  );
+    return (
+        <div className="min-h-screen flex flex-col items-center justify-center bg-blue-100">
+            <h1 className="text-4xl font-bold text-blue-700 mb-4">MERN Stack App</h1>
+            <p className="text-lg text-gray-700">{message}</p>
+        </div>
+    );
 }
 
 export default App;
